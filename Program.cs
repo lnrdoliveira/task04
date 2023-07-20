2fbbe26ca864ea5e5ddd0e0b19854efbfd4fe7c9
Leonardo <lnrdoliveira.dev@gmail.com>
2023-07-20 14:45:17 -0300

    Exercíco Retângulo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using task04;
+
+namespace Task04{
+    class Program{
+        static void Main(String[] args){
+            Retangulo r = new Retangulo();
+
+            System.Console.WriteLine("Entre com as informações do retângulo: ");
+            System.Console.Write("Largura: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.Write("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.WriteLine("Área: "+r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Perímetro: "+r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Diagonal: "+r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));     
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 Fazer um programa para ler os valores de largura e altura de um retângulo. Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal. Usar uma classe como mostrado no projeto.
 
     Retangulo
-- Largura: Double
-- Altura: Double
++ Largura: Double
++ Altura: Double
 _________________
 + Area(): double
 +Perimetro(): double
